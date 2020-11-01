56827f3fd0acfadecfc64a44677881f190e6723c
Eugeneche <53338476+Eugeneche@users.noreply.github.com>
2020-11-01 17:38:28 +0100

    Updates

---
+++ precache-manifest.c4f93b3ca63743cd621d3643833afbe4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b456c4fa179721727d375e7efd613c23",
+    "url": "/movies-search/index.html"
+  },
+  {
+    "revision": "742eb0872b44b8da1900",
+    "url": "/movies-search/static/css/2.10055323.chunk.css"
+  },
+  {
+    "revision": "6c5fd32b0aed5bf6b29c",
+    "url": "/movies-search/static/css/main.5e5cae3d.chunk.css"
+  },
+  {
+    "revision": "742eb0872b44b8da1900",
+    "url": "/movies-search/static/js/2.76543096.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-search/static/js/2.76543096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c5fd32b0aed5bf6b29c",
+    "url": "/movies-search/static/js/main.08b34103.chunk.js"
+  },
+  {
+    "revision": "3f0c3dc6467ca57c8ad3",
+    "url": "/movies-search/static/js/runtime-main.5f5da1d4.js"
+  },
+  {
+    "revision": "22c1c199e0730358618ae764b967501e",
+    "url": "/movies-search/static/media/background_1920.22c1c199.jpg"
+  },
+  {
+    "revision": "c2f608b6492315a9c969779fea245bac",
+    "url": "/movies-search/static/media/magnifier.c2f608b6.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/movies-search/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/movies-search/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/movies-search/static/media/revicons.2feb69cc.eot"
+  }
+]);
