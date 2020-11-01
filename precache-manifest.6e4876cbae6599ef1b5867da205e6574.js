e8175547dd16cefd54e78c1f6d17302f300c856a
Eugeneche <53338476+Eugeneche@users.noreply.github.com>
2020-11-01 17:52:23 +0100

    Updates

--- precache-manifest.6e4876cbae6599ef1b5867da205e6574.js
+++ precache-manifest.6e4876cbae6599ef1b5867da205e6574.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b456c4fa179721727d375e7efd613c23",
+    "revision": "febab593f48eebe84198bc28442cd236",
     "url": "/movies-search/index.html"
   },
   {
-    "revision": "742eb0872b44b8da1900",
+    "revision": "bebe4da5df32f49b8eca",
     "url": "/movies-search/static/css/2.10055323.chunk.css"
   },
   {
-    "revision": "6c5fd32b0aed5bf6b29c",
+    "revision": "fef070b46cdadfb5e58a",
     "url": "/movies-search/static/css/main.5e5cae3d.chunk.css"
   },
   {
-    "revision": "742eb0872b44b8da1900",
-    "url": "/movies-search/static/js/2.76543096.chunk.js"
+    "revision": "bebe4da5df32f49b8eca",
+    "url": "/movies-search/static/js/2.8a040e74.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/movies-search/static/js/2.76543096.chunk.js.LICENSE.txt"
+    "url": "/movies-search/static/js/2.8a040e74.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6c5fd32b0aed5bf6b29c",
-    "url": "/movies-search/static/js/main.08b34103.chunk.js"
+    "revision": "fef070b46cdadfb5e58a",
+    "url": "/movies-search/static/js/main.3b29c310.chunk.js"
   },
   {
     "revision": "3f0c3dc6467ca57c8ad3",
